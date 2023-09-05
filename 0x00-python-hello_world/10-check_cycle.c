8fc70de9d8c82bf4139cbefd3681304424723a45
Teeclever <omotoboraomotayo@gmail.com>
2023-09-04 18:20:31 -0700

    new

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * check_cycle - a program that check if there is cycle in list
+ * @list: the link to be checked
+ * Return: 1 if the list has cycle, 0 if not
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *fast = list;
+	listint_t *slow = list;
+
+	if (list != NULL)
+		return (0);
+
+	while (1)
+	{
+		if (fast->next != NULL && fast->next->next != NULL)
+		{
+			fast = fast->next->next;
+			slow = slow->next;
+
+			if (fast == slow)
+				return (1);
+		}
+		else
+			return (0);
+	}
+}
