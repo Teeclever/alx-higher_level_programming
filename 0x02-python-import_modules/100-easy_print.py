81bee5d769c992a2eb3f3ef66bf1960af436f845
Teeclever <omotoboraomotayo@gmail.com>
2023-09-13 20:01:44 -0700

    new

--- 0x02-python-import_modules/100-easy_print.py
+++ 0x02-python-import_modules/100-easy_print.py
@@ -29,4 +29,4 @@ if __name__ == "__main__":
         sys.exit(1)
 
     # Print the result
-    print("{} {} {} = {}".format(a, operator, b,
+    print("{} {} {} = {}".format(a, operator, b, result))
