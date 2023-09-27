d2c86624d211f6e740e8ffb85b652b86414489b0
Teeclever <omotoboraomotayo@gmail.com>
2023-09-26 20:07:14 -0700

    new

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""
+A class Square that defines a square by
+
+Private instance attribute
+
+"""
+
+
+class Square:
+    """
+    delearing the variables self and size.
+    to declear
+
+    """
+    def __init__(self, size=0):
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
