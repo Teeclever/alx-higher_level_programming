0c1ddfb8bb54cdeeb35c5d2ca3c1d1ff08ea0cf1
Teeclever <omotoboraomotayo@gmail.com>
2023-10-03 18:12:48 -0700

    implementinf everything is an object

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+    A class, LockedClass.
+"""
+
+
+class LockedClass:
+    """
+
+    Defines LockedClass which only allows creating of specific
+    class or object attributes.
+
+    """
+    __slots__ = ["first_name"]
