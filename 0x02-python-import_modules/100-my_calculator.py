34885754427cc4b8c406e00d31e1229bc9ea073d
Teeclever <omotoboraomotayo@gmail.com>
2023-10-28 20:02:39 -0700

    new

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,33 +1,32 @@
 #!/usr/bin/python3
 
-from calculator_1 import add, mul, div, sub
-from sys import argv, stderr, exit
-
+import sys
+from calculator_1 import add, sub, mul, div
 
 if __name__ == "__main__":
+    args = len(sys.argv)
 
-    store = argv
-    store.pop(0)
-
-    count = len(store)
-    if count != 3:
+    if args != 4:  # Check the number of arguments
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
-    result = 0
-    a = int(store[0])
-    operator = store[1]
-    b = int(store[2])
+        sys.exit(1)
 
-    match operator:
-        case '+':
-            result = add(a, b)
-        case '-':
-            result = sub(a, b)
-        case '/':
-            result = div(a, b)
-        case '*':
-            result = mul(a, b)
-        case _:
-            print("Unknown operator. Available operators: +, -, * and /")
-            exit(1)
-    print(f"{a} {operator} {b} = {result}")
+    # Extract arguments
+    a = int(sys.argv[1])
+    operator = sys.argv[2]
+    b = int(sys.argv[3])
+
+    # Perform the operation based on the operator
+    if operator == '+':
+        result = add(a, b)
+    elif operator == '-':
+        result = sub(a, b)
+    elif operator == '*':
+        result = mul(a, b)
+    elif operator == '/':
+        result = div(a, b)
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+
+    # Print the result
+    print("{} {} {} = {}".format(a, operator, b, result))
