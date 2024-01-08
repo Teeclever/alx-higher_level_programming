d5fbdd72f96f1646d4b59d3737485704fd8f0b21
Teeclever <omotoboraomotayo@gmail.com>
2024-01-08 23:56:56 +0100

    concat

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,10 +1,5 @@
 #!/usr/bin/node
-const { argv } = require('process');
 
-if (!argv[2]) {
-  console.log('undefined is undefined');
-} else if (!argv[3]) {
-  console.log(`${argv[2]} is undefined`);
-} else {
-  console.log(`${argv[2]} is ${argv[3]}`);
-}
+const myvar = process.argv.splice(2);
+
+console.log(`${myvar[0]} is ${myvar[1]}`);
