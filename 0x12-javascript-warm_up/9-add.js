b7d0e2329e36a9e88aea153db120798aea113c8e
Teeclever <omotoboraomotayo@gmail.com>
2024-01-09 00:44:28 +0100

    add

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,13 +1,10 @@
 #!/usr/bin/node
+const { argv } = require('process');
 
-const times = process.argv.splice(2);
+const num1 = parseInt(argv[2]);
+const num2 = parseInt(argv[3]);
+console.log(add(num1, num2));
 
 function add (a, b) {
   return a + b;
-}
-
-if (Number.isInteger(parseInt(times[0])) && Number.isInteger(parseInt(times[1]))) {
-  console.log(add(times[0], times[1]));
-} else {
-  console.log(NaN);
 }
